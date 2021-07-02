de4dc449948d6df9f9a848753611b3ed1b927a91
Isaac Frank <codingwithisaac@gmail.com>
2021-07-02 01:19:03 +0100

    install dependencies

--- test/test/sample-test.js
+++ test/test/sample-test.js
@@ -1,12 +1,18 @@
 const { expect } = require("chai");
 
 describe("Greeter", function() {
+
+  it("Should return 'Hello World", async function() {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+    await greeter.deployed();
+    expect(await greeter.greet()).to.equal("Hello, world!");
+  })
+  
   it("Should return the new greeting once it's changed", async function() {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
     await greeter.deployed();
-
-    expect(await greeter.greet()).to.equal("Hello, world!");
 
     const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
     
