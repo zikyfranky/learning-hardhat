2c44187dfb6567eb522737a5c3591ead872a4dc5
Isaac Frank <codingwithisaac@gmail.com>
2021-07-02 11:32:47 +0100

    Created first task

--- test/hardhat.config.js
+++ test/hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-web3");
+const hre = require("hardhat/config")
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -9,6 +11,17 @@ task("accounts", "Prints the list of accounts", async () => {
     console.log(account.address);
   }
 });
+
+task("balance", "Returns address' native balance")
+.addParam("address", "User address")
+.setAction(
+  async({address})=>{
+    const _address = web3.utils.toChecksumAddress(address)
+    const _balance = await web3.eth.getBalance(_address)
+    const _displayableBalance = web3.utils.fromWei(_balance)
+    console.log(`Balance:: ${_displayableBalance} ETH`);
+  }
+)
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
