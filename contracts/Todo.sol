bad74f734a41e614209e226a60a128097f0efde6
Isaac Frank <codingwithisaac@gmail.com>
2021-07-04 22:34:30 +0100

    Create getter functions for task/tasks

--- contracts/Todo.sol
+++ contracts/Todo.sol
@@ -15,7 +15,7 @@ contract Todo {
     mapping(address=>Task[]) usersTasks;
 
     /** 
-    * @notice function responsible for the creation of tasks
+    * @notice Function responsible for the creation of tasks
     * @param _task The task to be created
     * @param _owner  The author of the task
     */
@@ -29,5 +29,26 @@ contract Todo {
         // isDone value is false by default 
 
         usersTasks[msg.sender].push(task_);
+    }
+
+    
+    /** 
+    * @notice Function responsible for the fetching a user's specific task by id
+    * @param _user The creator of the task
+    * @param _id The index of the task
+    */
+
+    function getSingleUserTask(address _user, uint256 _id) public view returns(Task memory task_) {
+        task_ = usersTasks[_user][_id];
+        require(task_.date != 0 && task_.date <= block.timestamp, "Task with id doesn't exists");
+    }
+    
+    /** 
+    * @notice Function responsible for the fetching all user's
+    * @param _user The creator of the tasks
+    */
+
+    function getUserTasks(address _user) public view returns(Task[] memory tasks_) {
+        tasks_ = usersTasks[_user];
     }
 }
